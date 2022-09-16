188d5737418ca8943fe69610b526b55197bf482d
MD Ekramul Hoq <103028411+mdekramulhoqitf@users.noreply.github.com>
2022-09-16 23:27:24 +0200

    markup added hare

---
+++ js/teilweind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primery: "#FF7527",
+        black: "#203239",
+        overlay: "rgba(0,0,0,0.6)",
+        white: "#FFFFFF",
+      },
+      maxWidth: {
+        container: "1160px",
+      },
+      fontFamily: {
+        poppins: "Poppins",
+      },
+    },
+  },
+};
