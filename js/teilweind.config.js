d2537e5a8ed64fb526554b6ac8ecf6fc8c139a2e
MD Ekramul Hoq <103028411+mdekramulhoqitf@users.noreply.github.com>
2022-09-17 17:35:20 +0200

    nav%banner part

--- js/teilweind.config.js
+++ js/teilweind.config.js
@@ -2,10 +2,12 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        primery: "#FF7527",
+        blue: "#243c5a",
+        orange: "#ff6633",
         black: "#203239",
         overlay: "rgba(0,0,0,0.6)",
         white: "#FFFFFF",
+        bgcolor: "#EEEDDE",
       },
       maxWidth: {
         container: "1160px",
